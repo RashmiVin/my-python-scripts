26478844529c2020e0003c710a03c4ee8a244eda
RashmiVin <78325772+RashmiVin@users.noreply.github.com>
2021-02-27 17:14:56 -0500

    file commit

---
+++ First_python.py
@@ -0,0 +1,44 @@
+#Print a string
+print("Hello world")
+print("Congratulations on your first code")
+#Set variable and then print to see it on the screen
+a=1+2
+print(a)
+#Set variable using exponents
+b=a+17
+b=b**2
+print(b)
+#Set variable by using floating point
+c=7/5
+print(c)
+
+d='Rashmi'
+print(d)
+d='Rashmi\'s Macbook'
+print(d)
+f="Rashmi's iphone"
+print(f)
+e="Rashmi said \"Hello world\""
+print(e)
+g="""
+jghjhgjh\\n
+kjjkhjk
+mjhjkh
+dfsdsf
+fdgfgdgf
+"""
+print(g)
+h="what did you eat?\\nI had dose "
+print(h)
+print("My name is " "Rashmi")
+i="My name is " + "Rashmi"
+print(i)
+j=3*("My name is " + "Rashmi ")
+print(j)
+print(i[-5])
+print(i[11:5])
+print(len(j))
+k=[51,72,47,88]
+print(k[1])
+#To find the length of a list
+print(len(k))
