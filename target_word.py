d796bcab66605380fc16a917b56dde6d56a67148
RashmiVin <78325772+RashmiVin@users.noreply.github.com>
2021-04-06 08:52:26 -0400

    commit

--- target_word.py
+++ target_word.py
@@ -1,29 +1,42 @@
-
+import numpy as np
 def target_word_position(input_sentence,input_word):
     return_list = []
     input_sentence_list = input_sentence.split()
-    print(input_sentence_list)
+    #print(input_sentence_list)
     word_position = 1
     for i in input_sentence_list:
         #print(i, word_position)
         if i == input_word:
             return_list.append(word_position)
-            print(i, word_position)
+            #print(i, word_position)
         word_position = word_position + 1
 
-
+    print(input_word," ",return_list)
     return return_list
+
+def convert(input_string):
+    new_dict = {}
+    input_sentence_list = input_string.split()
+    #print(input_sentence_list)
+    x = np.array(input_sentence_list)
+    unique_word_list = np.unique(x)
+    #print(unique_word_list)
+    for each_word in unique_word_list:
+        word_position = target_word_position(input_string,each_word)
+        count_word = len(word_position)
+        #print(each_word, " ", word_position," ",count_word)
+        new_dict[each_word] = count_word
+    return new_dict
+
+
+
+
+
 
 
 inp_sentence = "I love NJ as NJ has a lot of good restaurant within NJ"
 inp_word = "NJ"
-output_list = target_word_position(inp_sentence,inp_word)
-print(output_list)
+#output_list = target_word_position(inp_sentence,inp_word)
+#print(output_list)
+print(convert(inp_sentence))
 
-def convert(list_to_dict):
-    new_result = iter(list_to_dict)
-    result_dict = dict(zip(new_result, new_result))
-    return result_dict
-
-nr = ['I', 'love', 'NJ', 'as', 'NJ', 'has', 'a', 'lot', 'of', 'good', 'restaurant', 'within', 'NJ']
-print(convert(nr))
